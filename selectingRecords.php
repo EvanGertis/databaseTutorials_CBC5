1389590a15c4df5bed484505f2526d40bb890e4e
EvanGertis <39931491+EvanGertis@users.noreply.github.com>
2019-02-20 11:53:02 -0500

    covered fetch mode

---
+++ selectingRecords.php
@@ -0,0 +1,23 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "employees";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $stmt = $conn->prepare("SELECT id, firstname, lastname FROM myguys"); 
+    $stmt->execute();
+
+    // set the resulting array to associative
+    $result = $stmt->setFetchMode(PDO::FETCH_ASSOC); 
+    foreach ($stmt->fetchAll() as $key => $value) {
+        echo 'employee: '.$value['firstname'].' '.$value['lastname'].'<br>';
+    }
+}
+catch(PDOException $e) {
+    echo "Error: " . $e->getMessage();
+}
+$conn = null;
+?>
